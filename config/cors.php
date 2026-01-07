89765094fa27c24a4ff3a621822e77c62b3dc290
EDMAR <edmarcrescencio856@gmail.com>
2026-01-07 13:08:55 +0800

    Updated and fixed bugs

--- config/cors.php
+++ config/cors.php
@@ -12,11 +12,21 @@ return [
     */
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        // Production frontend domains
+        'https://mhrpci.site',
+        'https://www.mhrpci.site',
+        // Local dev frontends
+        'http://127.0.0.1:8000',
+        'http://localhost:8000',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+        'http://192.168.1.210:4000',
+        'http://192.168.1.210:4001',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    // Use wildcard origins, so credentials must be disabled per CORS spec
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];

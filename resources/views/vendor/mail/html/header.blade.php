a93167f00addfb0f4d4e8de57432bf8e2aa1a061
EDMAR <edmarcrescencio856@gmail.com>
2026-01-08 13:47:06 +0800

    Updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === config('app.name'))
-<img src="{{ asset('images/logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-width: 200px; height: auto;">
+<img src="https://mhrpci.site/images/mhrhci.png" class="logo" alt="{{ config('app.name') }}" style="max-width: 200px; height: auto;">
 @else
 {{ $slot }}
 @endif
---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,57 @@
+<x-mail::message>
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('Hello!')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('Regards,')<br>
+{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+<x-slot:subcopy>
+@lang(
+    "If you're having trouble clicking the \":actionText\" button, click here:",
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all"><a href="{{ $actionUrl }}" style="color:#0b5ed7;">Click Here</a></span>
+</x-slot:subcopy>
+@endisset
+</x-mail::message>
